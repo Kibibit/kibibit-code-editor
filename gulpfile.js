7f85b06aeedff322ee163e7a53204554a2b5ed95
Neil Kalman <Neilkalman@gmail.com>
2015-09-16 14:07:48 +0300

    [Build Tools] Added formatter for front-end files
    Adds a format gulp task that formats our .js, .scss, and .html files by our standard formatting.
    
    Next task is to add the same for backend and make a new task that runs both

--- gulpfile.js
+++ gulpfile.js
@@ -24,6 +24,57 @@ gulp.task('lint-sass', function() {
 
 gulp.task('lint', ['lint-js', 'lint-sass']);
 
+gulp.task('format-front-end', function() {
+	return gulp.src([
+		/* Ignore folders which are from external libraries that might contain html, javascript, or css/sass*/
+		'!./public/assets/lib/', // exclude libraries folder
+		'!./public/assets/lib/**', // exclude anything in the libraries folder
+		/* JavaScript */
+		'./public/**/*.js', // front-end javascript
+		/* HTML */
+		'./public/**/*.html', // front-end javascript
+		/* SASS */
+		'./public/assets/sass/**/*.scss',
+	])
+	.pipe(prettify({
+	    //config: "path/to/.jsbeautifyrc",
+	    html: {
+	        braceStyle: "collapse",
+	        indentChar: " ",
+	        indentScripts: "keep",
+	        indentSize: 4,
+	        maxPreserveNewlines: 10,
+	        preserveNewlines: true,
+	        /* unformatted: ["a", "sub", "sup", "b", "i", "u"], */
+	        wrapLineLength: 0
+	    },
+	    css: {
+	        indentChar: " ",
+	        indentSize: 4
+	    },
+	    js: {
+	        braceStyle: "collapse",
+	        breakChainedMethods: false,
+	        e4x: false,
+	        evalCode: false,
+	        indentChar: " ",
+	        indentLevel: 0,
+	        indentSize: 4,
+	        indentWithTabs: false,
+	        jslintHappy: false,
+	        keepArrayIndentation: false,
+	        keepFunctionIndentation: false,
+	        maxPreserveNewlines: 10,
+	        preserveNewlines: true,
+	        spaceBeforeConditional: true,
+	        spaceInParen: false,
+	        unescapeStrings: false,
+	        wrapLineLength: 0
+	    }
+	}))
+	.pipe(gulp.dest('./dist')); // add this to a different folder in order to test first
+});
+
 gulp.task('styles', function() {
 	return gulp.src('./public/assets/sass/**/*.scss')
 	.pipe(sourcemaps.init())
