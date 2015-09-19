506f9bdd95776d41e3889a4a06f86770050aeadb
Neil Kalman <neilkalman@gmail.com>
2015-09-19 12:47:48 +0300

    [Formatting] format front-end with the new gulp task

--- public/app/app.js
+++ public/app/app.js
@@ -3,7 +3,7 @@ angular.module('userApp', ['ngAnimate', 'app.routes', 'authService', 'mainCtrl',
 // application configuration to integrate token into requests
 .config(function($httpProvider) {
 
-	// attach our auth interceptor to the http requests
-	$httpProvider.interceptors.push('AuthInterceptor');
+    // attach our auth interceptor to the http requests
+    $httpProvider.interceptors.push('AuthInterceptor');
 
 });
--- public/app/app.routes.js
+++ public/app/app.routes.js
@@ -2,42 +2,42 @@ angular.module('app.routes', ['ngRoute'])
 
 .config(function($routeProvider, $locationProvider) {
 
-	$routeProvider
+    $routeProvider
 
-		// route for the home page
-		.when('/', {
-			templateUrl : 'app/views/pages/home.html'
-		})
-		
-		// login page
-		.when('/login', {
-			templateUrl : 'app/views/pages/login.html',
-   			controller  : 'mainController',
-    			controllerAs: 'login'
-		})
-		
-		// show all users
-		.when('/users', {
-			templateUrl: 'app/views/pages/users/all.html',
-			controller: 'userController',
-			controllerAs: 'user'
-		})
+    // route for the home page
+        .when('/', {
+        templateUrl: 'app/views/pages/home.html'
+    })
 
-		// form to create a new user
-		// same view as edit page
-		.when('/users/create', {
-			templateUrl: 'app/views/pages/users/single.html',
-			controller: 'userCreateController',
-			controllerAs: 'user'
-		})
+    // login page
+    .when('/login', {
+        templateUrl: 'app/views/pages/login.html',
+        controller: 'mainController',
+        controllerAs: 'login'
+    })
 
-		// page to edit a user
-		.when('/users/:user_id', {
-			templateUrl: 'app/views/pages/users/single.html',
-			controller: 'userEditController',
-			controllerAs: 'user'
-		});
+    // show all users
+    .when('/users', {
+        templateUrl: 'app/views/pages/users/all.html',
+        controller: 'userController',
+        controllerAs: 'user'
+    })
 
-	$locationProvider.html5Mode(true);
+    // form to create a new user
+    // same view as edit page
+    .when('/users/create', {
+        templateUrl: 'app/views/pages/users/single.html',
+        controller: 'userCreateController',
+        controllerAs: 'user'
+    })
+
+    // page to edit a user
+    .when('/users/:user_id', {
+        templateUrl: 'app/views/pages/users/single.html',
+        controller: 'userEditController',
+        controllerAs: 'user'
+    });
+
+    $locationProvider.html5Mode(true);
 
 });
