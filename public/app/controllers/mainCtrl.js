d054c0983772ab498412e2dfc5edc62be2a59bf6
Or Tichon <or.tichon@wispear.com>
2015-09-23 02:34:24 +0300

    button to get the Development folder content

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -4,21 +4,29 @@ angular.module('mainCtrl', ['ui.ace'])
 
     var vm = this;
 
-$rootScope.code = "alert('hello world');";
+    $rootScope.code = "alert('hello world');";
 
     $rootScope.editorOptions = {
         mode: 'javascript',
         theme: 'monokai'
     };
 
-    $http.get('/api/directory/' + encodeURIComponent('/home/thatkookooguy/Downloads/test'))
-        .then(function(res) {
-            console.log(res.errno);
-            if (res.errno !== null) {
-                $rootScope.folder = res.data;
-                console.log('got res');
-            }
-        });
+    $rootScope.chooseFolder = 'Choose Folder'
+
+    vm.openFolder = function(){
+        $rootScope.chooseFolder = 'Working...'
+        $http.get('/api/directory/' + encodeURIComponent('/Users/Tichon/Development'))
+            .then(function(res) {
+                console.log(res.errno);
+                if (res.errno !== null) {
+                    $rootScope.folder = res.data;
+                    $rootScope.chooseFolder = 'Choose Folder'
+                    console.log('got res');
+                }
+            }); 
+    }
+
+
 
     $rootScope.treeOptions = {
         nodeChildren: "children",
