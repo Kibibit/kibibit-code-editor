4e44a656737a9050f8b880b023053e1ca7716937
Neil Kalman <neilkalman@gmail.com>
2016-01-04 23:08:50 +0200

    remove unwanted submitted code + configure debug mode
    in order to remove the scope classes

--- public/app/app.js
+++ public/app/app.js
@@ -5,4 +5,7 @@ angular.module('kibibitCodeEditor',
 	'treeControl',
 	'ui.ace',
 	'angularResizable',
-	'angularModalService']);
+	'angularModalService'])
+.config(['$compileProvider', function($compileProvider) {
+  $compileProvider.debugInfoEnabled(false);
+}]);
