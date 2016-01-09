329ff547b18a3b4f86474374a0a109985b5b2325
Neil Kalman <neilkalman@gmail.com>
2016-01-09 16:05:03 +0200

    change the text

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,11 @@
-angular.module('kibibitCodeEditor',
-	['angular-loading-bar',
-	'ngAnimate',
-	'app.routes',
-	'treeControl',
-	'ui.ace',
-	'angularResizable',
-	'angularModalService'])
-.config(['$compileProvider', function($compileProvider) {
-  $compileProvider.debugInfoEnabled(false);
-}]);
+angular.module('kibibitCodeEditor', ['angular-loading-bar',
+        'ngAnimate',
+        'app.routes',
+        'treeControl',
+        'ui.ace',
+        'angularResizable',
+        'angularModalService'
+    ])
+    .config(['$compileProvider', function($compileProvider) {
+        $compileProvider.debugInfoEnabled(false);
+    }])
