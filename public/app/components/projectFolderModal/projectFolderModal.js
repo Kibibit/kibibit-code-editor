35b55b525530f6eeb8368850a083984d4c2064d8
Or Tichon <or.tichon@gmail.com>
2016-04-08 16:08:10 +0300

    fixed #35 - file tree encapsulation
    ## summary
    * code editor component gets the selected file path, get it's content and update the view
    * file tree component gets the folder path, load the folder structure and update the tree
    * main controller now acts as a "bridge" between the file tree and code editor components

--- public/app/components/projectFolderModal/projectFolderModal.js
+++ public/app/components/projectFolderModal/projectFolderModal.js
@@ -1,29 +1,7 @@
 angular.module('kibibitCodeEditor')
 
 .controller('projectFolderModalController',[
-  '$rootScope',
-  'FolderService',
-   function(
-    $rootScope,
-    FolderService) {
+  function() {
 
-     var vm = this;
-
-     vm.setCurrentFolder = function(node) {
-    vm.currentFolder = node.path;
-  };
-
-     // open the chosen project folder
-     vm.openFolder = function() {
-    vm.code = null;
-    vm.projectFolder = false;
-    console.log(vm.currentFolder);
-    FolderService.getFolder(vm.currentFolder, function(res) {
-      vm.workFolder = res.data;
-      console.log('got res: ' + res);
-      $rootScope.$emit('workFolderSelected', vm.workFolder);
-    });
-    return true;
-  };
-
-   }]);
+    var vm = this;
+  }]);
