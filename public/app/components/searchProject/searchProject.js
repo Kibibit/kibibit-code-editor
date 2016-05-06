c962952de3a23f3827ee868a9397e670d12bef7d
Neil Kalman <Neilkalman@gmail.com>
2016-05-06 05:54:09 +0200

    implement search in menu bar

---
+++ public/app/components/searchProject/searchProject.js
@@ -0,0 +1,54 @@
+angular.module('kibibitCodeEditor')
+
+.directive('kbSearchProject', function() {
+  return {
+    scope: {},
+    bindToController: {
+      searchTerm: '=ngModel'
+    },
+    controller: 'searchProjectController',
+    controllerAs: 'searchProjectCtrl',
+    templateUrl: 'app/components/searchProject/searchProjectTemplate.html',
+    link: function(scope, element, attrs, searchProjectCtrl) {
+      var searchBtn = angular.element('.search-btn');
+      var input = angular.element('.search-input');
+
+      /* OPEN SEARCH */
+      searchBtn.click(openSearch);
+
+      /* BLUR ON ESC */
+      input.keyup(function(e) {
+          if (e.keyCode == 27) {
+              input.blur();
+          }
+      });
+
+      /* CHANGE CLASSES ON FOCUS\BLUR */
+      input.focus(function() {
+          input.addClass('open');
+          searchBtn.addClass('open');
+          input.removeClass('blurred');
+      });
+
+      input.blur(function() {
+          if (input.val() === '') {
+              input.removeClass('open');
+              searchBtn.removeClass('open');
+          }
+          input.addClass('blurred');
+      });
+
+      function openSearch() {
+          if (!input.hasClass('open')) {
+              input.focus();
+          }
+      }
+    }
+  };
+})
+
+.controller('searchProjectController', [
+  function() {
+    var vm = this;
+  }
+]);
