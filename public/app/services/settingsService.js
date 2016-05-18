be2d561f204ad7c7dccbe77d6f22f3e6b1e8d3af
or <or.tichon@wispear.com>
2016-05-18 17:27:25 +0300

    added mode/theme list constants

--- public/app/services/settingsService.js
+++ public/app/services/settingsService.js
@@ -1,252 +1,256 @@
 angular.module('kibibitCodeEditor')
 
-.service('SettingsService', ['Fullscreen', function(Fullscreen) {
-  var vm = this;
+.service('SettingsService', [
+  'Fullscreen',
+  'CODE_EDITOR',
+  function(
+    Fullscreen,
+    CODE_EDITOR) {
 
-  /* init */
-  vm.settings = new Settings();
+    var vm = this;
 
-  function Settings() {
-    var settings = this;
-    /* INIT */
-    var cursor = {row: '0', column: '0'};
-    var isFullscreen = false;
-    var currentUndoManager = undefined;
-    var currentEditor = undefined;
-    var modelist = ace.require('ace/ext/modelist');
-    var themelist = ace.require('ace/ext/themelist');
-    var editorSettings = new EditorSettings();
+    /* init */
+    vm.settings = new Settings();
 
-    /* EXPOSE SIMPLE VARS */
-    // This are probably better off in state instead of settings. but they're here for now :-)
-    settings.cursor = cursor;
-    settings.currentUndoManager = currentUndoManager;
-    settings.currentEditor = currentEditor;
-    settings.modelist = modelist;
-    settings.themelist = themelist;
+    function Settings() {
+      var settings = this;
+      /* INIT */
+      var cursor = {row: '0', column: '0'};
+      var isFullscreen = false;
+      var currentUndoManager = undefined;
+      var currentEditor = undefined;
+      var editorSettings = new EditorSettings();
 
-    settings.__defineSetter__('isFullscreen', function(newValue) {
+      /* EXPOSE SIMPLE VARS */
+      // This are probably better off in state instead of settings. but they're here for now :-)
+      settings.cursor = cursor;
+      settings.currentUndoManager = currentUndoManager;
+      settings.currentEditor = currentEditor;
+      settings.modelist = CODE_EDITOR.MODE_LIST;
+      settings.themelist = CODE_EDITOR.THEME_LIST;
 
-      console.assert(isBoolean(newValue), {
-        'message': 'isFullscreen should be boolean',
-        'currentValue': isFullscreen,
-        'newValue': newValue
+      settings.__defineSetter__('isFullscreen', function(newValue) {
+
+        console.assert(isBoolean(newValue), {
+          'message': 'isFullscreen should be boolean',
+          'currentValue': isFullscreen,
+          'newValue': newValue
+        });
+
+        if (newValue !== currentFullscreenState()) {
+          if (newValue) {
+            Fullscreen.all();
+          } else {
+            Fullscreen.cancel();
+          }
+        }
       });
 
-      if (newValue !== currentFullscreenState()) {
-        if (newValue) {
-          Fullscreen.all();
-        } else {
-          Fullscreen.cancel();
-        }
+      settings.__defineGetter__('isFullscreen', function() {
+        return currentFullscreenState();
+      });
+
+      settings.__defineGetter__('editorSettings', function() {
+        return editorSettings;
+      });
+
+      function EditorSettings() {
+        var theme = 'monokai';
+        var ruler = 80;
+        var tabWidth = 4;
+        var fontSize = 12;
+        var isGutter = true;
+        var lineWrap = false;
+        var isReadOnly = false;
+        var isSoftTabs = false;
+        var syntaxMode = 'javascript';
+
+        this.__defineGetter__('ruler', function() {
+          return ruler;
+        });
+
+        this.__defineSetter__('ruler', function(newValue) {
+          console.assert(Number.isInteger(newValue), {
+            'message': 'ruler should be integer',
+            'currentValue': ruler,
+            'newValue': newValue
+          });
+          if (newValue !== ruler) {
+            if (settings.currentEditor) {
+              var editor = settings.currentEditor;
+              editor.setShowPrintMargin(newValue != 0);
+              editor.setPrintMarginColumn(newValue);
+            }
+            ruler = newValue;
+          }
+        });
+
+        this.__defineGetter__('lineWrap', function() {
+          return lineWrap;
+        });
+
+        this.__defineSetter__('lineWrap', function(newValue) {
+          console.assert(isBoolean(newValue), {
+            'message': 'lineWrap should be boolean',
+            'currentValue': lineWrap,
+            'newValue': newValue
+          });
+          if (newValue !== lineWrap) {
+            if (settings.currentEditor) {
+              var session = settings.currentEditor.getSession();
+              session.setUseWrapMode(newValue);
+            }
+            lineWrap = newValue;
+          }
+        });
+
+        this.__defineGetter__('fontSize', function() {
+          return fontSize;
+        });
+
+        this.__defineSetter__('fontSize', function(newValue) {
+          console.assert(Number.isInteger(newValue), {
+            'message': 'fontSize should be integer',
+            'currentValue': fontSize,
+            'newValue': newValue
+          });
+
+          if (newValue !== fontSize) {
+            if (settings.currentEditor) {
+              var editor = settings.currentEditor;
+              editor.setFontSize(newValue);
+            }
+            fontSize = newValue;
+          }
+        });
+
+        this.__defineGetter__('tabWidth', function() {
+          return tabWidth;
+        });
+
+        this.__defineSetter__('tabWidth', function(newValue) {
+          console.assert(Number.isInteger(newValue), {
+            'message': 'tabWidth should be integer',
+            'currentValue': tabWidth,
+            'newValue': newValue
+          });
+
+          if (newValue !== tabWidth) {
+            if (settings.currentEditor) {
+              var session = settings.currentEditor.getSession();
+              session.setTabSize(newValue);
+            }
+            tabWidth = newValue;
+          }
+        });
+
+        this.__defineGetter__('isSoftTabs', function() {
+          return isSoftTabs;
+        });
+
+        this.__defineSetter__('isSoftTabs', function(newValue) {
+          console.assert(isBoolean(newValue), {
+            'message': 'isSoftTabs should be integer',
+            'currentValue': isSoftTabs,
+            'newValue': newValue
+          });
+
+          if (newValue !== isSoftTabs) {
+            if (settings.currentEditor) {
+              var session = settings.currentEditor.getSession();
+              session.setUseSoftTabs(newValue);
+            }
+            isSoftTabs = newValue;
+          }
+        });
+
+        this.__defineGetter__('isGutter', function() {
+          return isGutter;
+        });
+
+        this.__defineSetter__('isGutter', function(newValue) {
+          console.assert(isBoolean(newValue), {
+            'message': 'isGutter should be boolean',
+            'currentValue': isGutter,
+            'newValue': newValue
+          });
+
+          if (newValue !== isGutter) {
+            if (settings.currentEditor) {
+              var editor = settings.currentEditor;
+              editor.renderer.setShowGutter(newValue);
+            }
+          }
+          isGutter = newValue;
+        });
+
+        this.__defineGetter__('syntaxMode', function() {
+          return syntaxMode;
+        });
+
+        this.__defineSetter__('syntaxMode', function(newValue) {
+          console.assert(angular.isString(newValue), {
+            'message': 'syntaxMode should be string',
+            'currentValue': syntaxMode,
+            'newValue': newValue
+          });
+
+          if (newValue !== syntaxMode) {
+            if (settings.currentEditor) {
+              var session = settings.currentEditor.getSession();
+              session.setMode('ace/mode/' + newValue);
+            }
+          }
+          syntaxMode = newValue;
+        });
+
+        this.__defineGetter__('theme', function() {
+          return theme;
+        });
+
+        this.__defineSetter__('theme', function(newValue) {
+          console.assert(angular.isString(newValue), {
+            'message': 'theme should be string',
+            'currentValue': theme,
+            'newValue': newValue
+          });
+
+          if (newValue !== theme) {
+            if (settings.currentEditor) {
+              var editor = settings.currentEditor;
+              editor.setTheme('ace/theme/' + newValue);
+            }
+          }
+          theme = newValue;
+        });
+
+        this.__defineGetter__('isReadOnly', function() {
+          return isReadOnly;
+        });
+
+        this.__defineSetter__('isReadOnly', function(newValue) {
+          console.assert(isBoolean(newValue), {
+            'message': 'isReadOnly should be boolean',
+            'currentValue': isReadOnly,
+            'newValue': newValue
+          });
+
+          if (newValue !== isReadOnly) {
+            var editor = settings.currentEditor;
+            editor.setReadOnly(newValue);
+          }
+          isReadOnly = newValue;
+        });
+
       }
-    });
-
-    settings.__defineGetter__('isFullscreen', function() {
-      return currentFullscreenState();
-    });
-
-    settings.__defineGetter__('editorSettings', function() {
-      return editorSettings;
-    });
-
-    function EditorSettings() {
-      var theme = 'monokai';
-      var ruler = 80;
-      var tabWidth = 4;
-      var fontSize = 12;
-      var isGutter = true;
-      var lineWrap = false;
-      var isReadOnly = false;
-      var isSoftTabs = false;
-      var syntaxMode = 'javascript';
-
-      this.__defineGetter__('ruler', function() {
-        return ruler;
-      });
-
-      this.__defineSetter__('ruler', function(newValue) {
-        console.assert(Number.isInteger(newValue), {
-          'message': 'ruler should be integer',
-          'currentValue': ruler,
-          'newValue': newValue
-        });
-        if (newValue !== ruler) {
-          if (settings.currentEditor) {
-            var editor = settings.currentEditor;
-            editor.setShowPrintMargin(newValue != 0);
-            editor.setPrintMarginColumn(newValue);
-          }
-          ruler = newValue;
-        }
-      });
-
-      this.__defineGetter__('lineWrap', function() {
-        return lineWrap;
-      });
-
-      this.__defineSetter__('lineWrap', function(newValue) {
-        console.assert(isBoolean(newValue), {
-          'message': 'lineWrap should be boolean',
-          'currentValue': lineWrap,
-          'newValue': newValue
-        });
-        if (newValue !== lineWrap) {
-          if (settings.currentEditor) {
-            var session = settings.currentEditor.getSession();
-            session.setUseWrapMode(newValue);
-          }
-          lineWrap = newValue;
-        }
-      });
-
-      this.__defineGetter__('fontSize', function() {
-        return fontSize;
-      });
-
-      this.__defineSetter__('fontSize', function(newValue) {
-        console.assert(Number.isInteger(newValue), {
-          'message': 'fontSize should be integer',
-          'currentValue': fontSize,
-          'newValue': newValue
-        });
-
-        if (newValue !== fontSize) {
-          if (settings.currentEditor) {
-            var editor = settings.currentEditor;
-            editor.setFontSize(newValue);
-          }
-          fontSize = newValue;
-        }
-      });
-
-      this.__defineGetter__('tabWidth', function() {
-        return tabWidth;
-      });
-
-      this.__defineSetter__('tabWidth', function(newValue) {
-        console.assert(Number.isInteger(newValue), {
-          'message': 'tabWidth should be integer',
-          'currentValue': tabWidth,
-          'newValue': newValue
-        });
-
-        if (newValue !== tabWidth) {
-          if (settings.currentEditor) {
-            var session = settings.currentEditor.getSession();
-            session.setTabSize(newValue);
-          }
-          tabWidth = newValue;
-        }
-      });
-
-      this.__defineGetter__('isSoftTabs', function() {
-        return isSoftTabs;
-      });
-
-      this.__defineSetter__('isSoftTabs', function(newValue) {
-        console.assert(isBoolean(newValue), {
-          'message': 'isSoftTabs should be integer',
-          'currentValue': isSoftTabs,
-          'newValue': newValue
-        });
-
-        if (newValue !== isSoftTabs) {
-          if (settings.currentEditor) {
-            var session = settings.currentEditor.getSession();
-            session.setUseSoftTabs(newValue);
-          }
-          isSoftTabs = newValue;
-        }
-      });
-
-      this.__defineGetter__('isGutter', function() {
-        return isGutter;
-      });
-
-      this.__defineSetter__('isGutter', function(newValue) {
-        console.assert(isBoolean(newValue), {
-          'message': 'isGutter should be boolean',
-          'currentValue': isGutter,
-          'newValue': newValue
-        });
-
-        if (newValue !== isGutter) {
-          if (settings.currentEditor) {
-            var editor = settings.currentEditor;
-            editor.renderer.setShowGutter(newValue);
-          }
-        }
-        isGutter = newValue;
-      });
-
-      this.__defineGetter__('syntaxMode', function() {
-        return syntaxMode;
-      });
-
-      this.__defineSetter__('syntaxMode', function(newValue) {
-        console.assert(angular.isString(newValue), {
-          'message': 'syntaxMode should be string',
-          'currentValue': syntaxMode,
-          'newValue': newValue
-        });
-
-        if (newValue !== syntaxMode) {
-          if (settings.currentEditor) {
-            var session = settings.currentEditor.getSession();
-            session.setMode('ace/mode/' + newValue);
-          }
-        }
-        syntaxMode = newValue;
-      });
-
-      this.__defineGetter__('theme', function() {
-        return theme;
-      });
-
-      this.__defineSetter__('theme', function(newValue) {
-        console.assert(angular.isString(newValue), {
-          'message': 'theme should be string',
-          'currentValue': theme,
-          'newValue': newValue
-        });
-
-        if (newValue !== theme) {
-          if (settings.currentEditor) {
-            var editor = settings.currentEditor;
-            editor.setTheme('ace/theme/' + newValue);
-          }
-        }
-        theme = newValue;
-      });
-
-      this.__defineGetter__('isReadOnly', function() {
-        return isReadOnly;
-      });
-
-      this.__defineSetter__('isReadOnly', function(newValue) {
-        console.assert(isBoolean(newValue), {
-          'message': 'isReadOnly should be boolean',
-          'currentValue': isReadOnly,
-          'newValue': newValue
-        });
-
-        if (newValue !== isReadOnly) {
-          var editor = settings.currentEditor;
-          editor.setReadOnly(newValue);
-        }
-        isReadOnly = newValue;
-      });
-
     }
-  }
 
-  function isBoolean(value) {
-    return value === true || value === false;
-  }
+    function isBoolean(value) {
+      return value === true || value === false;
+    }
 
-  function currentFullscreenState() {
-    return $(window).data('fullscreen-state');
-  }
+    function currentFullscreenState() {
+      return $(window).data('fullscreen-state');
+    }
 
 }]);
