5b357217babe1be94ca2ce4e77aa36e9ab9f3c12
or <or.tichon@wispear.com>
2016-05-18 18:20:44 +0300

    assertion message is now angular constant

--- public/app/app.js
+++ public/app/app.js
@@ -58,4 +58,12 @@ angular.module('kibibitCodeEditor',
 .constant('CODE_EDITOR', {
   'MODE_LIST': ace.require('ace/ext/modelist'),
   'THEME_LIST': ace.require('ace/ext/themelist')
+})
+.constant('TYPE_ERROR_MSGS', function(varName, typeExpected, typeRecieved) {
+  return [
+    varName,
+    ' should be a ',
+    typeExpected,
+    ' but was given a ',
+    typeRecieved].join('');
 });
