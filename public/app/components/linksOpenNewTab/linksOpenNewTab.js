80941e5682a7a747fb79f0c63b35ca8175908054
Neil Kalman <Neilkalman@gmail.com>
2016-05-23 00:37:05 +0200

    lint

--- public/app/components/linksOpenNewTab/linksOpenNewTab.js
+++ public/app/components/linksOpenNewTab/linksOpenNewTab.js
@@ -6,8 +6,9 @@ angular.module('kibibitCodeEditor')
       scope.$watch(function() {
         return element.html();
       }, function() {
-        var elems = (element.prop("tagName") === 'A') ? element : element.find('a');
-        elems.attr("target", "_blank");
+        var elems =
+          (element.prop('tagName') === 'A') ? element : element.find('a');
+        elems.attr('target', '_blank');
       });
     }
   };
