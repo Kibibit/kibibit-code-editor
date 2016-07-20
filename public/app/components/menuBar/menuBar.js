e75cc2dd2174d41a77c0a23822fc74fcf964f0ac
Neil Kalman <Neilkalman@gmail.com>
2016-07-21 02:40:03 +0300

    add some more ace integrations
    + some case conversation buttons

--- public/app/components/menuBar/menuBar.js
+++ public/app/components/menuBar/menuBar.js
@@ -13,7 +13,8 @@ angular.module('kibibitCodeEditor')
     if (!str) { return; }
     var keys = str.split('-');
     var isOSX = /Mac OS X/.test($window.navigator.userAgent);
-    var seperator = (!isOSX || keys.length > 2) ? '+' : '';
+    var seperator =
+      (!isOSX || keys.length > 2 || keys[0] === '_Ctrl') ? '+' : '';
     var abbreviations = {
       M: isOSX ? '⌘' : 'Ctrl',
       A: isOSX ? 'Option' : 'Alt',
@@ -21,7 +22,8 @@ angular.module('kibibitCodeEditor')
     };
     return keys.map(function(key, index) {
       var last = index == keys.length - 1;
-      return last ? key : abbreviations[key];
+      return key.startsWith('_') ?
+        key.slice(1) : (last ? key : abbreviations[key]);
     }).join(seperator);
   };
 })
@@ -100,6 +102,118 @@ angular.module('kibibitCodeEditor')
   vm.toggleGutter = function() {
     var editorSettings = vm.settings.editorSettings;
     editorSettings.isGutter = !editorSettings.isGutter;
+  };
+
+  vm.changeSelectionToCamelCase = function() {
+    var editor = vm.settings.currentEditor;
+    var selectionText = editor.getSelectedText();
+    var selection = vm.settings.currentEditor.selection.getRange();
+    var camelCased = selectionText.replace(
+        /[_-\s]([a-zA-Z])/g,
+        function (g) {
+            return g[1].toUpperCase();
+        }
+    );
+    camelCased = camelCased[0].toLowerCase() + camelCased.substring(1);
+    vm.settings.currentEditor.session.replace(
+    selection, camelCased);
+
+    editor.focus();
+  };
+
+  vm.changeSelectionToTitleCase = function() {
+    var editor = vm.settings.currentEditor;
+    var selectionText = editor.getSelectedText();
+    var selection = vm.settings.currentEditor.selection.getRange();
+    var titleCased = selectionText.replace(
+      /[_-\s]([a-zA-Z])|([a-z])([A-Z])/g,
+      function (g, singleLetter, firstLetter, secondLetter) {
+        return secondLetter ?
+          firstLetter + ' ' + secondLetter.toUpperCase() :
+          ' ' + singleLetter.toUpperCase();
+      }
+    );
+
+    titleCased = titleCased[0].toUpperCase() + titleCased.substring(1);
+    vm.settings.currentEditor.session.replace(
+    selection, titleCased);
+
+    editor.focus();
+  };
+
+  vm.changeSelectionToKebabCase = function() {
+    var editor = vm.settings.currentEditor;
+    var selectionText = editor.getSelectedText();
+    var selection = vm.settings.currentEditor.selection.getRange();
+    var titleCased = selectionText.replace(
+      /[_-\s]([a-zA-Z])|([a-z])([A-Z])/g,
+      function (g, singleLetter, firstLetter, secondLetter) {
+        return secondLetter ?
+          firstLetter + '-' + secondLetter.toLowerCase() :
+          '-' + singleLetter.toLowerCase();
+      }
+    );
+
+    titleCased = titleCased[0].toLowerCase() + titleCased.substring(1);
+    vm.settings.currentEditor.session.replace(
+    selection, titleCased);
+
+    editor.focus();
+  };
+
+  vm.changeSelectionToSnakeCase = function() {
+    var editor = vm.settings.currentEditor;
+    var selectionText = editor.getSelectedText();
+    var selection = vm.settings.currentEditor.selection.getRange();
+    var titleCased = selectionText.replace(
+      /[_-\s]([a-zA-Z])|([a-z])([A-Z])/g,
+      function (g, singleLetter, firstLetter, secondLetter) {
+        return secondLetter ?
+          firstLetter + '_' + secondLetter.toLowerCase() :
+          '_' + singleLetter.toLowerCase();
+      }
+    );
+
+    titleCased = titleCased[0].toLowerCase() + titleCased.substring(1);
+    vm.settings.currentEditor.session.replace(
+    selection, titleCased);
+
+    editor.focus();
+  };
+
+  vm.execEditorCommand = function(command) {
+    var editor = vm.settings.currentEditor;
+    if (editor) {
+      editor.execCommand(command);
+      editor.focus();
+    }
+  };
+
+  vm.isSelectionExists = function() {
+    var editor = vm.settings.currentEditor;
+    return editor ? editor.getSelectedText() !== '' : false;
+  };
+
+  vm.canChangeCase = function() {
+    var selectionText = vm.settings.currentEditor ?
+      vm.settings.currentEditor.getSelectedText() : '';
+
+    return /[-_\sA-Z]/.test(selectionText);
+
+  };
+
+  vm.isMultiLineSelection = function() {
+    var selectionRange = vm.settings.currentEditor ?
+      vm.settings.currentEditor.getSelectionRange() : undefined;
+
+    if (!selectionRange) {
+      return;
+    }
+
+    var startLine = selectionRange.start.row;
+    var endLine = selectionRange.end.row;
+
+    return endLine > startLine;
   };
 
 });
