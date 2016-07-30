f5adf19e6b0be223174ec5c268791c97aaed07e4
Neil Kalman <Neilkalman@gmail.com>
2016-07-30 11:25:09 +0300

    nowwwwwww

--- public/example projects/batman-project/app/models/Nerd.js
+++ public/example projects/batman-project/app/models/Nerd.js
@@ -4,5 +4,5 @@ var mongoose = require('mongoose');
 // define our nerd model
 // module.exports allows us to pass this to other files when it is called
 module.exports = mongoose.model('Nerd', {
-  name: {type : String, default: ''}
+  name: {type: String, default: ''}
 });
