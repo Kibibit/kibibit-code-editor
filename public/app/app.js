d7640b91238c633c08178ee2328d8576605ee543
Neil Kalman <Neilkalman@gmail.com>
2016-08-01 16:49:11 +0300

    make menu icon morph into back arrow
    + fixes like:
    - no more duplication of sidebar

--- public/app/app.js
+++ public/app/app.js
@@ -14,7 +14,8 @@ angular.module('kibibitCodeEditor',
   'emoji',
   'jsonFormatter',
   'ngclipboard',
-  'ng.deviceDetector'])
+  'ng.deviceDetector',
+  'ngMdIcons'])
 
 .config(['$compileProvider', function($compileProvider) {
   $compileProvider.debugInfoEnabled(false);
