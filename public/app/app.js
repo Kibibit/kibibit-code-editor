12a48ad811c3a9510df79283c5ff4cd85bd3f995
Or Tichon <or.tichon@gmail.com>
2016-09-03 11:49:20 +0100

    moved constants from app.js to their own file

--- public/app/app.js
+++ public/app/app.js
@@ -58,30 +58,4 @@ angular.module('kibibitCodeEditor',
   return function(input) {
     return marked(input || '');
   };
-}])
-
-.constant('CODE_EDITOR', {
-  'MODE_LIST': ace.require('ace/ext/modelist'),
-  'THEME_LIST': ace.require('ace/ext/themelist')
-})
-  
-.constant('DRAFTS', {
-  'DRAFT_PREFIX' : 'draft-'
-})  
-  
-.constant('ERROR_MSGS', {
-  'TYPE_ERROR': function(varName, typeExpected, typeRecieved) {
-    return [
-      varName,
-      ' should be a ',
-      typeExpected,
-      ' but was given a ',
-      typeRecieved].join('');
-  },
-  'MATCH_ERROR': function(varName, collectionName) {
-    return [
-      varName,
-      'value should exist in ',
-      collectionName].join('');
-  }
-});
+}]);
