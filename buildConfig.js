84c33c3a3fd1e3e7091f20fe7e66c459745ef4dd
Neil Kalman <Neilkalman@gmail.com>
2016-09-06 22:44:37 +0300

    refine our build settings

--- buildConfig.js
+++ buildConfig.js
@@ -8,15 +8,19 @@ var buildConfig = {
       path: './server.js'//,
       //execArgv: ['--harmony']
     },
+    distServer: {
+      path: './server.js',
+      execArgv: ['--dist']
+    },
     bs: {
       proxy: {
         target: 'http://localhost:3141',
         middleware: function (req, res, next) {
-          console.log(req.url);
+          //console.log(req.url);
           next();
         }
       },
-      files: ['./public/**/*'], // files to watch with bs instantly (.ejs & .css)
+      files: ['./public/**/*.css'], // files to watch with bs instantly (.ejs & .css)
       logLevel: 'silent'
     }
   },
@@ -33,6 +37,6 @@ buildConfig.FILES.SERVER_JS_WITHOUT_MAIN = ['./app/**/*.js', './config.js'];
 buildConfig.FILES.SERVER_JS = [].concat(buildConfig.FILES.SERVER_MAIN, buildConfig.FILES.SERVER_JS_WITHOUT_MAIN);
 buildConfig.FILES.BUILD_FILES = ['./gulpfile.js'];
 buildConfig.FILES.JS_ALL = [].concat(buildConfig.FILES.FRONTEND_JS, buildConfig.FILES.SERVER_JS);
-buildConfig.FILES.LINT = [].concat(buildConfig.FILES.FRONTEND_JS, buildConfig.FILES.SERVER_JS_WITHOUT_MAIN);
+buildConfig.FILES.LINT_JS = [].concat(buildConfig.FILES.FRONTEND_JS, buildConfig.FILES.SERVER_JS_WITHOUT_MAIN);
 
 module.exports = buildConfig;
